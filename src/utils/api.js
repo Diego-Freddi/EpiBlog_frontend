9119a139a54e00d6bd2686ce4defdaa587c07fcf
Diego F <diego.freddi@icloud.com>
2025-03-27 19:56:11 +0100

    Aggiornato baseURL in api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5020/api'
+    baseURL: 'https://epiblog-backend-8vvs.onrender.com/api'
 });
 
 // Aggiungi il token a tutte le richieste
